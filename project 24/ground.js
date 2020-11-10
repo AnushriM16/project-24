e04fdfa5aa26e791c6fc4b9e1a8dad545d466ef9
AnushriM16 <72220555+AnushriM16@users.noreply.github.com>
2020-11-10 18:03:06 +0000

    Add files via upload

---
+++ project 24/ground.js
@@ -0,0 +1,19 @@
+class ground{
+constructor(x,y,width,height){
+    var options={
+        isStatic:true
+    }
+    this.bodies=Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    World.add(world,this.body);
+}
+display() {
+    var pos = this.body.position;
+          rectMode(CENTER);
+          fill("yellow");
+          rect(pos.x, pos.y, this.width, this.height);
+
+}
+}
+
